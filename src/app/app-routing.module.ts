1d476f79c48f0430091f6283f554c7b1664c296d
rafaelaishi <rafaelaishi@hotmail.com>
2018-10-06 11:26:52 -0300

    Iniciando

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { LoginComponent } from "./login/login.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { Routes, RouterModule} from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+
+export const APP_ROUTES:Routes=[
+
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'cadastro',component:CadastroComponent},
+];
+
+export const routing:ModuleWithProviders=RouterModule.forRoot(APP_ROUTES);
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+import { routing } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    routing,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
